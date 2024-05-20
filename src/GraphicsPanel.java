e3f29123cfad26b52c9a80a8992f284db079044d
BT_2N2_22 <sample@bths.edu>
2024-05-20 12:52:53 -0400

    Made GraphicsPanel class

---
+++ src/GraphicsPanel.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GraphicsPanel extends JPanel implements KeyListener, MouseListener, ActionListener {
+
+    public GraphicsPanel() {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
